fa70c7900820f8fb1dc5e279a65e1e282d064110
Sanju Kushwaha <126675045+Sanjukushwaha101@users.noreply.github.com>
2023-03-01 17:48:23 +0530

    Create Student.java
    in this code we create a simple class use object with help constructor

---
+++ Student.java
@@ -0,0 +1,22 @@
+# oops-program
+class student{
+	String name;
+	int id;
+	
+	public void stufun(){
+		System.out.println(this.name);
+		System.out.println(this.id);
+		}
+		}
+		
+	student(String name, int id){
+		this.name=name;
+		this.id=id
+		}
+public class Student{
+	public ststic void main(String[] args){
+		student s1 = new student("sanju",01);
+		
+		s1.student();
+	}
+	}
