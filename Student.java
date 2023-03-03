181358072921daa19d660e351dab11bfe6aeb722
Sanju Kushwaha <126675045+Sanjukushwaha101@users.noreply.github.com>
2023-03-04 03:57:47 +0530

    Update Student.java
    we some change in this code

--- Student.java
+++ Student.java
@@ -1,5 +1,6 @@
 # oops-program
 class student{
+
 	String name;
 	int id;
 	
@@ -7,14 +8,13 @@ class student{
 		System.out.println(this.name);
 		System.out.println(this.id);
 		}
-		}
-		
 	student(String name, int id){
 		this.name=name;
 		this.id=id
 		}
+	}	
 public class Student{
-	public ststic void main(String[] args){
+	public static void main(String[] args){
 		student s1 = new student("sanju",01);
 		
 		s1.student();
